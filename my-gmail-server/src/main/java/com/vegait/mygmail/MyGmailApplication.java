4b7a58d9b5d04127eba43771460d80e915654312
Olga Savic <o.savic@vegait.rs>
2021-02-08 16:24:33 +0100

    Project initialization

---
+++ my-gmail-server/src/main/java/com/vegait/mygmail/MyGmailApplication.java
@@ -0,0 +1,13 @@
+package com.vegait.mygmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGmailApplication.class, args);
+	}
+
+}
---
+++ my-gmail-server/src/test/java/com/vegait/mygmail/MyGmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegait.mygmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
